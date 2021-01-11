9958353cc377d506e4028a9969e5ee111577a304
Corentin <corentin.vernel@etu.unilim.fr>
2021-01-11 11:50:56 +0100

    supprimer modifier

--- include/pages/insererTabExcel.inc.php
+++ include/pages/insererTabExcel.inc.php
@@ -1,122 +1,123 @@
 
 
 <h1>Ajouter des variables</h1>
-<a href="include/pages/SaisiFormules.inc.php">Saisir des formules</a>
+
 <?php
-/*
+
 require_once("classes/variable.class.php");
 require_once("classes/variableManager.class.php");
+if(empty($_POST["Nom"])){
+  ?>
 
-?>
-
-<form action ="#" method = "post" enctype="multipart/form-data" id = "insererTabExcell">
-  <input type="file" name="upload_file"></br>
-  <input type="submit" name="Valider" value="Valider">
-
-</form>
-<?php
-
-function lire_csv($nom_fichier){
-
-  $ligne = array();
-  $fichier = fopen($nom_fichier,'r');
-  $compteur = 1;
-
-  $i = 'ligne'.$compteur;
-  $taille=filesize($nom_fichier)+1;
-
-
-
-  while(!feof($fichier)){
-
-      $donnee = fgets($fichier);
-      if(!feof($fichier)){
-      $ligne[$i]=explode(";",$donnee);
-
-      $compteur++;
-      $i = 'ligne'.$compteur;
-    }
-    }
-
-  fclose($fichier);
-  return $ligne;
+  <form action ="#" method = "post" enctype="multipart/form-data" id = "insererTabExcell">
+    Nom du fichier :<input type="text" name="Nom"></br>
+    <input type="file" name="upload_file"></br>
+    <input type="submit" name="Valider" value="Valider">
+  </form>
+  <?php
 }
 
-$fileName = $_FILES["upload_file"]["name"];
-$fileExt = ".". strtolower(substr(strrchr($fileName,'.'), 1));
-$tmpName = $_FILES["upload_file"]["tmp_name"];
-$uniqueName = "Bonjour";
-$fileName = "Excel/".$uniqueName.$fileExt;
-$result = move_uploaded_file($tmpName, $fileName);
+        function lire_csv($nom_fichier){
+
+          $ligne = array();
+          $fichier = fopen($nom_fichier,'r');
+          $compteur = 1;
+
+          $i = 'ligne'.$compteur;
+          $taille=filesize($nom_fichier)+1;
 
 
-if (isset($_POST["submit"])){
-  $maxSize = 50000;
-  $validExt=array('.xls');
-  if($_FILES['uploaded_file']['error']>0){
-    echo "Une erreur est survenue";
-    die;
-  }
 
-  $fileSize = $_FILES['uploaded_file']['size'];
+          while(!feof($fichier)){
 
-  if ($fileSize>$maxSize){
-    echo "Le fichier est trop gros";
-    die;
-  }
+            $donnee = fgets($fichier);
+            if(!feof($fichier)){
+              $ligne[$i]=explode(";",$donnee);
 
-  $fileName=$_FILES['uploaded_file']['name'];
-  $fileExt='.'.strolower(substr(strrchr($fileName,'.'),1));
+              $compteur++;
+              $i = 'ligne'.$compteur;
+            }
+          }
 
-  if (!in-array($fileExt,$validExt)){
-    echo "Le fichier n'est pas un tableau Excel";
-    die;
-  }
+        fclose($fichier);
+        return $ligne;
+      }
+if(!empty($_POST["Nom"])){
+  echo "bonjour";
+  $fileName = $_FILES["upload_file"]["name"];
+  $fileExt = ".". strtolower(substr(strrchr($fileName,'.'), 1));
+  $tmpName = $_FILES["upload_file"]["tmp_name"];
+  $uniqueName = $_POST["Nom"];
+  $fileName = "Excel/".$uniqueName.$fileExt;
+  $result = move_uploaded_file($tmpName, $fileName);
 
-  $tmpName=$_FILES['uploaded_file']['tmp_name'];
-  $fileName="Excel/".$fileName.$fileExt;
-  $resultat=move_uploaded_file($tmpName,$fileName);
-  if($resultat){
-    echo "Transfert terminé";
-  }
-} ?>
 
-<table>
-  <tr>
-    <?php
-    $db =new PDO("mysql:host=localhost; dbname=pt_gmp","root","");
-    $donnees = lire_csv('Excel/'.$uniqueName.$fileExt);
+  if (isset($_POST["submit"])){
+    $maxSize = 50000;
+    $validExt=array('.xls');
+    if($_FILES['uploaded_file']['error']>0){
+      echo "Une erreur est survenue";
+      die;
+    }
 
-    $k = 0;
+    $fileSize = $_FILES['uploaded_file']['size'];
 
-    $j = "variable".($k + 1);
+    if ($fileSize>$maxSize){
+      echo "Le fichier est trop gros";
+      die;
+    }
 
-    $variableManager = new VariableManager($db);
-    foreach ($donnees as $value){
+    $fileName=$_FILES['uploaded_file']['name'];
+    $fileExt='.'.strolower(substr(strrchr($fileName,'.'),1));
 
-      while($k < 9){
-      $tab[$j]= $value[$k];
-      $k++;
+    if (!in-array($fileExt,$validExt)){
+      echo "Le fichier n'est pas un tableau Excel";
+      die;
+    }
+
+    $tmpName=$_FILES['uploaded_file']['tmp_name'];
+    $fileName="Excel/".$fileName.$fileExt;
+    $resultat=move_uploaded_file($tmpName,$fileName);
+    if($resultat){
+      echo "Transfert terminé";
+    }
+  } ?>
+
+  <table>
+    <tr>
+      <?php
+      $db =new PDO("mysql:host=localhost; dbname=pt_gmp","root","");
+      $donnees = lire_csv('Excel/'.$uniqueName.$fileExt);
+
+      $k = 0;
+
       $j = "variable".($k + 1);
-    }
 
-    $variable = new Variable($tab);
-    $result =$variableManager->add($variable);
+      $variableManager = new VariableManager($db);
+      foreach ($donnees as $value){
 
-    $k = 0;
-    $j = "variable".($k + 1);
-    }
-/*
-    $variable = new Variable();
-    $result =$variableManager->add($variable);
+        while($k < 9){
+          $tab[$j]= $value[$k];
+          $k++;
+          $j = "variable".($k + 1);
+        }
 
-*/
+        $variable = new Variable($tab);
+        $result =$variableManager->add($variable);
 
+        $k = 0;
+        $j = "variable".($k + 1);
+      }
+      /*
+      $variable = new Variable();
+      $result =$variableManager->add($variable);
+
+      */
+
+      ?>
+
+    </tr>
+  </table>
+  <?php
+}
 ?>
-
-</tr>
-</table>
-<?php
-
-  ?>
-*/
