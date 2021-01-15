ee674ad2e3b758952f516014e0ad2a79faceee99
Léo Mazabraud <leo.mazabraud@iut.unilim.fr>
2021-01-15 09:29:51 +0100

    cplmt information et css + image logo

--- include/pages/informations.inc.php
+++ include/pages/informations.inc.php
@@ -1,1 +1,29 @@
 <?php //page relative au groupe et au contexte du projet ?>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../css/style_css.css" />
+</head>
+<body>
+    <div id="logo">
+        <img src="../../image/LOGO%20FIVE%20GUYS%20DEV.png">
+    </div>
+    <div class="bloc"> <!-- premier bloc -->
+        <h1>Qui sommes-nous?</h1>
+        <div>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+    </div>
+    <div> <!-- premier bloc -->
+        <h1>Quel est notre objectif?</h1>
+        <div class="bloc">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+    </div>
+</body>
+
