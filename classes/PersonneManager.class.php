ec645cc869f59bd0882e4797b168b80ee84289c0
Corentin <corentin.vw@sfr.fr>
2021-02-04 12:24:36 +0100

    personneManager

--- classes/PersonneManager.class.php
+++ classes/PersonneManager.class.php
@@ -2,43 +2,81 @@
 class PersonneManager{
 	private $db;
 
-    public function __construct($db){
+  public function __construct($db){
         $this->db = $db;
-    }
-    public function getIdPersonne($email, $mot_de_passe){
-          $sql = "SELECT id_personne FROM personne WHERE email = '".$email."' and mot_de_passe = '".$mot_de_passe."' ";
-          $requete = $this->db->prepare($sql);
-          $requete->execute();
-          return $requete->fetch();
-    }
+  }
+	public function add($personne){
 
-    public function estEtudiant($id_personne){
-          $sqlEtudiant = "SELECT id_personne, etu_nom, etu_prenom, annee FROM etudiant where id_personne =". $id_personne."";
-          $requeteEtudiant = $this->db->prepare($sqlEtudiant);
-          $requeteEtudiant->execute();
+	    $requete = $this->db->prepare(
+	      'INSERT INTO personne (per_mail,per_mdp) VALUES (:per_mail, :per_mdp);');
+	      $requete->bindValue(':per_mail',$personne->getPersonneEmail());
+	      $requete->bindValue(':per_mdp',$personne->getPersonneMotDePasse());
+	      $retour=$requete->execute();
+	      $num = $this->db->lastInsertID();
+	      return $num;
+	    }
+			public function RecupPersNum($per_mail){
 
-          while ($personne = $requeteEtudiant->fetch(PDO::FETCH_ASSOC)){
-              if($id_personne == $personne['id_personne']){
-                  return true;
-                  $requeteEtudiant->closeCursor();
-              }
-          }
-          return false;
-          $requeteEtudiant->closeCursor();
-    }
+			      $sql = 'select per_num FROM personne where per_mail="'.$per_mail.'"';
+			      $requete = $this->db->prepare($sql);
+			      $requete->execute();
+			      return $requete->fetch();
+			    }
+	public function getIdPersonne($email, $mot_de_passe){
+	$sql = "SELECT per_num FROM personne
+					WHERE per_mail = '".$email."' and per_mdp = '".$mot_de_passe."' ";
 
-    public function estProfesseur($id_personne){
-          $sqlEtudiant = "SELECT id_personne, pro_nom, pro_prenom FROM professeur where id_personne =". $id_personne."";
-          $requeteEtudiant = $this->db->prepare($sqlEtudiant);
-          $requeteEtudiant->execute();
+	$requete = $this->db->prepare($sql);
+	$requete->execute();
+	return $requete->fetch();
+}
 
-          while ($personne = $requeteEtudiant->fetch(PDO::FETCH_ASSOC)){
-              if($id_personne == $personne['id_personne']){
-                  return true;
-                  $requeteEtudiant->closeCursor();
-              }
-          }
-          return false;
-          $requeteEtudiant->closeCursor();
-    }
-} ?>
+public function EstPresent($per_mail,$per_mdp){
+		$sql = 'select per_mail,per_mdp FROM personne WHERE per_mail= "'.$per_mail.'"';
+		$requete = $this->db->prepare($sql);
+		$requete->execute();
+
+		while ($personne = $requete->fetch(PDO::FETCH_ASSOC)){
+			if($per_mail == $personne['per_mail'] && $per_mdp ==$personne['per_mdp']){
+				return true;
+
+				$requete->closeCursor();
+			}
+		}
+		return false;
+		$requete->closeCursor();
+
+	}
+
+	public function estEtudiant($id_personne){
+		$sqlEtudiant = "SELECT id_personne, etu_nom, etu_prenom, annee FROM etudiant where id_personne =". $id_personne."" ;
+		$requeteEtudiant = $this->db->prepare($sqlEtudiant);
+	  $requeteEtudiant->execute();
+
+		while ($personne = $requeteEtudiant->fetch(PDO::FETCH_ASSOC)){
+		        if($id_personne == $personne['id_personne']){
+		          return true;
+
+		          $requeteEtudiant->closeCursor();
+		        }
+		      }
+		      return false;
+		      $requeteEtudiant->closeCursor();
+		}
+
+	public function estProfesseur($id_personne){
+		$sqlEtudiant = "SELECT id_personne, pro_nom, pro_prenom FROM professeur where id_personne =". $id_personne."" ;
+		$requeteEtudiant = $this->db->prepare($sqlEtudiant);
+		$requeteEtudiant->execute();
+
+		while ($personne = $requeteEtudiant->fetch(PDO::FETCH_ASSOC)){
+						if($id_personne == $personne['id_personne']){
+							return true;
+
+							$requeteEtudiant->closeCursor();
+						}
+					}
+					return false;
+					$requeteEtudiant->closeCursor();
+				}
+}
