ad4b4700617d691ba1a8e660a327c6bee36e0358
Corentin <corentin.vw@sfr.fr>
2021-02-04 12:23:42 +0100

    reconf

--- include/config.inc.php
+++ include/config.inc.php
@@ -3,11 +3,7 @@
 // A modifier en fonction de la configuration
 
 define('DBHOST', "localhost");
-<<<<<<< HEAD
 define('PORT', "3306"); // à changer SI MySQL utilise un autre port (à vérifier dans PhpMyadmin) Léo --> 3307
-=======
-define('PORT', "3307"); // à changer SI MySQL utilise un autre port (à vérifier dans PhpMyadmin) Léo --> 3307
->>>>>>> 27a029f2fc33e164bbf425007a44cca7fabcae11
 define('DBNAME', "pt_gmp");
 define('DBUSER', "root");
 define('DBPASSWD', "");
