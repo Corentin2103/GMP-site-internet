4e743b3c52620b71a69e8d775cec733ead80a609
Léo Mazabraud <leo.mazabraud@iut.unilim.fr>
2021-02-04 11:39:59 +0100

    modif css + aj rappel

--- include/pages/page_eleve.inc.php
+++ include/pages/page_eleve.inc.php
@@ -8,24 +8,31 @@
 </head>
 <body>
 <!-- A U C U N   P H P   P O U R   L E   M O M E N T -->
-    <!-- Quesiton 1 -->
-    <div class="question">
-        <h1>Question 1</h1>
-        <div class="sous-question">
-            <input type="number" id="answer1" name="answer" placeholder="Votre réponse">
-            <select name=unite" id="unite-select1">
-                <option value="">  Choisir l'unité  </option>
-                <option value="metre">m</option>
-                <option value="newton">N</option>
-                <option value="newtion_par_metre">N.m²</option>
-            </select>
-        </div>
-        <div class="buttons">
-            <div class="container">
-                <button class="btn effect01" target="_blank"><span>Enregistrer</span></button>
-            </div>
+<!-- Quesiton 1 -->
+<div class="question">
+    <h1>Question 1</h1>
+    
+    <div class="rap">
+        <h2>Rappel du sujet</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere felis a auctor faucibus. Suspendisse accumsan, urna sed lobortis finibus, urna urna aliquet quam, pulvinar pellentesque orci mauris sed tortor. Donec molestie ligula sed urna volutpat faucibus non eget urna. Nam rhoncus ullamcorper neque, eu luctus ex malesuada in. Mauris porta dictum turpis at molestie.</p>
+    </div>
+
+    <div class="sous-question">
+        <input type="number" id="answer1" name="answer" placeholder="Votre réponse">
+        <select name=unite" id="unite-select1">
+            <option value="">  Choisir l'unité  </option>
+            <option value="metre">m</option>
+            <option value="newton">N</option>
+            <option value="newtion_par_metre">N.m²</option>
+        </select>
+    </div>
+    <!-- bouton enregistrer -->
+    <div class="buttons">
+        <div class="container">
+            <button class="btn effect01" target="_blank"><span>Enregistrer</span></button>
         </div>
     </div>
+</div>
 
     <!-- Quesiton 2 -->
     <div class="question">
