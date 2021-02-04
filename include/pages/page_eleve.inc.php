28f71923389a4f6e758a613c8adc992ce12d5ca4
Léo Mazabraud <leo.mazabraud@iut.unilim.fr>
2021-02-04 11:22:07 +0100

    Debut page élève

---
+++ include/pages/page_eleve.inc.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page élève</title>
+    <link rel="stylesheet" type="text/css" href="../../css/style_css.css" />
+    <script type="text/javascript" src="../../javascript/script.js"></script>
+</head>
+<body>
+<!-- A U C U N   P H P   P O U R   L E   M O M E N T -->
+    <!-- Quesiton 1 -->
+    <div class="question">
+        <h1>Question 1</h1>
+        <div class="sous-question">
+            <input type="number" id="answer1" name="answer" placeholder="Votre réponse">
+            <select name=unite" id="unite-select1">
+                <option value="">  Choisir l'unité  </option>
+                <option value="metre">m</option>
+                <option value="newton">N</option>
+                <option value="newtion_par_metre">N.m²</option>
+            </select>
+        </div>
+        <div class="buttons">
+            <div class="container">
+                <button class="btn effect01" target="_blank"><span>Enregistrer</span></button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Quesiton 2 -->
+    <div class="question">
+        <h1>Question 2</h1>
+        <div class="sous-question">
+            <input type="number" id="answer2" name="answer" placeholder="Votre réponse">
+            <select name=unite" id="unite-select2">
+                <option value="">  Choisir l'unité  </option>
+                <option value="metre">m</option>
+                <option value="newton">N</option>
+                <option value="newtion_par_metre">N.m²</option>
+            </select>
+        </div>
+        <div class="buttons">
+            <div class="container">
+                <button class="btn effect01" target="_blank"><span>Enregistrer</span></button>
+            </div>
+        </div>
+    </div>
+</body>
